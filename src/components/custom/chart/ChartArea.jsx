7fb3c82ada751ec08f797c8bd34c1c2d7592d8f5
Michael Fernandes <fernb0t@Kaia-Lynnes-MBP.localdomain>
2020-12-01 19:23:01 -0800

    Add labels to custom chart

--- src/components/custom/chart/ChartArea.jsx
+++ src/components/custom/chart/ChartArea.jsx
@@ -2,38 +2,54 @@ import React from 'react';
 import { useCallback, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
-import { arc, min, select, max } from 'd3';
-import { generativeData, multiply, shapeData,  DATA_DIMENSIONS, PAD_ANGLE, SPACE_BETWEEN_ARCS, COLORS } from '../../../util';
-import { getPreData, getPostData, getLabels } from '../../../store/selectors';
+import { arc, min, select } from 'd3';
+import { PAD_ANGLE, SPACE_BETWEEN_ARCS, COLORS, INNER_RADIUS } from '../../../util';
+import {  getSessionData } from '../../../store/selectors';
+
+const pi = Math.PI;
+
+console.log(pi / 4);
+const radians = (degrees) => degrees * (pi/180);
 
 
+const xRadiusFunc = (radius, theta, slice) => 
+  (radius) * Math.cos(theta * (slice) - (pi / 4));
 
-function ChartArea() {
+const xRadius = (radius, theta, slice) => {
+  const xR = xRadiusFunc(radius, theta, slice);
+  console.log(xR);
+  return xR;
+}
+
+const yRadius = (radius, theta, slice) => 
+ (radius) * Math.sin(theta * (slice) - (pi / 4));
+
+function ChartArea({ width, height}) {
   const ref = useRef();
-
-  const preData = useSelector(getPreData);
-  const dims = preData;
-  const partitions = Object.keys(preData).length - 1; 
-  const shapedData = shapeData(preData);
-
-  // const postData = useSelector(getPostData);
-  // const labels = useSelector(getLabels);
-
-  const width = 600;
-  const height = 600;
+  const textRef = useRef();
+  
+  const data = useSelector(getSessionData);
+  const dims = data.data;
+  const slices = data.labels.length;
+  const partitions = 6; 
 
   const sq = min([width, height]);
-  const radius = sq / 2
+  const half = sq / 2
+  const radius = half / 2;
+  const theta = radians(360 / slices);
 
-  const innerRadiusBuffer = 25;
-  const ringWidth = (radius - innerRadiusBuffer) / (partitions + 1);
-  
+  const innerRadiusBuffer = 10;
+  const ringWidth = (radius - INNER_RADIUS) / (partitions);
+
   const getAngle = useCallback(
-    (partition) => ((2 * Math.PI / Object.keys(dims).length ) * partition)
-  , [dims]);
-
+    (partition) => ((2 * Math.PI / slices ) * partition)
+  , [slices]);
+  
+  const getAnchor = useCallback(
+    (slice) => 
+  , [theta])
   const arcGen = useCallback((slice, level) => {
-    let startRadius = (level * ringWidth) + innerRadiusBuffer;
+    let startRadius = (level * ringWidth) + INNER_RADIUS;
     return arc()
       .innerRadius(startRadius + SPACE_BETWEEN_ARCS)
       .outerRadius(startRadius + ringWidth)
@@ -42,22 +58,17 @@ function ChartArea() {
       .padAngle(PAD_ANGLE)
       .padRadius(radius)
       .cornerRadius(1)();
-  }, [getAngle, ringWidth, radius, dims]);
+  }, [getAngle, ringWidth ]);
 
-  // const makeCentroid = (level, slice) => {
-  //   let startRadius = (level * ringWidth) + innerRadiusBuffer;
-  //   var centroid = arc().innerRadius(startRadius + 0.25)
+  // const makeCentroid = (slice, level) => {
+  //   let startRadius = (level * ringWidth) + INNER_RADIUS;
+  //   return arc().innerRadius(startRadius + 0.25)
   //     .outerRadius(startRadius + ringWidth)
   //     .startAngle(getAngle(slice))
   //     .endAngle(getAngle(slice + 1))
   //     .padAngle(PAD_ANGLE)
   //     .padRadius(radius)
-  //     .cornerRadius(1);
-  //   return centroid;
-  // }
-
-  // const mouseOver = (d,i ) => {
-  //   console.log(d,i);
+  //     .cornerRadius(1)();
   // }
 
   const enterRadar = useCallback((enter) => {
@@ -67,34 +78,64 @@ function ChartArea() {
         .attr("transform", `translate(${width / 2},${height / 2})`)
         .attr("d", d => arcGen(d.slice, d.level))
         .attr('opacity', d =>  d.active ? 1.0 : 0.2)
-        .attr('color', d =>  console.log(d))
         .attr('fill', d => COLORS[d.level])
-        .attr('stroke', d => d.active ? 'grey' : 'none')
-        .attr('stroke-width', d => d.active ? '0.5px' : 'none')
         .attr('className', d => `${d.name}`);
-        // .on('mouseover', (d) => console.log(d));
   }, [arcGen, height, width]);
 
-  const updateRadar = useCallback((update) => {
-    update
-      .attr('fill', 'orange');
+  const updateRadar = useCallback((enter) => {
+    enter
+        .attr("transform", `translate(${width / 2},${height / 2})`)
+        .attr("d", d => arcGen(d.slice, d.level))
+        .attr('opacity', d =>  d.active ? 1.0 : 0.2)
+        .attr('fill', d => COLORS[d.level])
   }, [arcGen, height, width]);
+
+  const textUpdate = useCallback((update) => 
+    update
+        .attr("x", (d) => xRadius(radius, theta, d.slice ))
+        .attr('y', (d) => yRadius(radius, theta, d.slice ))
+        .attr('color', (d, i) => 'orange' )
+        .attr('text-anchor', d =>  
+          radians(theta, d.slice) < radians(360/2) ? 'start' : 'end'
+        )
+        // .attr('fill', (d, i) => console.log(d) )
+        .text (d => `${d.slice + d.name}`)
+    , [radius, theta]);
+
+  const textEnter = useCallback((enter) => 
+    enter
+      .append('text')
+      .attr("transform", `translate(${width / 2},${height / 2})`)
+        .attr('key', d => d)
+        .attr('class', 'labels')
+        .attr('font-size', 12)
+        .attr('text-anchor', 'start')
+        .text (d => `${d.slice + d.name}`)
+        .attr("x", (d) => xRadius(radius, theta, d.slice ))
+        .attr('y', (d) => yRadius(radius, theta, d.slice ))
+  , [slices, width, height, radians, theta]);
 
   useEffect(()=> {
-    if(ref.current){
+    if(ref.current && data.data){
       select(ref.current)
       .selectAll('path')
-      .data(shapedData, ({key}) => key)
-      .join(enterRadar, updateRadar => updateRadar, exit => exit.remove());
+      .data(data.data, ({key}) => key)
+      .join(enterRadar, updateRadar, exit => exit.remove());
     }
-  }, [ width, height, enterRadar, shapedData ]);
+
+    if(textRef.current && data.labels) {
+      select(textRef.current) // rays
+        .selectAll(".labels")
+        .data(data.labels, key => key)
+        .join(textEnter, textUpdate, exit => exit.remove());
+    }
+  }, [ width, height, enterRadar, data.data, data.labels ]);
 
   return (
-    <div className="chart-wrapper">
-      <svg width={width} height={height} className="chart" >
-        <g className="concentric-radar"  ref={ref} />
-      </svg>
-    </div>
+    <svg width={width} height={height} className="chart" >
+      <g className="concentric-radar"  ref={ref} />
+      <g className="labels" ref={textRef} />
+    </svg>
   );
 }
 
